a752da1d33c0cfe52316a790a7e1f3d63a9f492c
alexkolotukhina <alex_kolotukhina@outlook.com>
2018-02-16 15:46:40 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$('.ba-slider').slick({
+		'infinite': true,
+	  	'speed': 300,
+	  	'slidesToShow': 1,
+	  	'variableWidth': true,
+	  	'focusOnSelect': true
+	});
+})
